bc90b92b0c119285ac7c3b7aaa66613cbd7cdf17
odhiambb <odhiambb@gmail.com>
2022-07-05 08:10:25 +0300

    working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { latLng, LatLng, tileLayer, circle, polygon } from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'naapukMaa';
+  showAddEvent = false;
+  options = {
+    layers: [
+      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: '...',
+      }),
+    ],
+    zoom: 10,
+    center: latLng(46.879966, -121.726909),
+  };
+  layersControl = {
+    baseLayers: {
+      'Open Street Map': tileLayer(
+        'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        { maxZoom: 18, attribution: '...' }
+      ),
+      'Open Cycle Map': tileLayer(
+        'http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png',
+        { maxZoom: 18, attribution: '...' }
+      ),
+    },
+    overlays: {
+      'Big Circle': circle([46.95, -122], { radius: 5000 }),
+      'Big Square': polygon([
+        [46.8, -121.55],
+        [46.9, -121.55],
+        [46.9, -121.7],
+        [46.8, -121.7],
+      ]),
+    },
+  };
+  ngOnInit() {}
+  addEvent() {
+    this.showAddEvent = !this.showAddEvent;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, LeafletModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
